2ae977ca7646bee59f2cc3af2948fc3c319a0290
Naw-R <rowannasser14@gmail.com>
2025-01-16 15:54:52 -0500

    Task 2 Updated

--- Tasks/Week 1/Task 2/js/script.js
+++ Tasks/Week 1/Task 2/js/script.js
@@ -11,8 +11,9 @@
 
 "use strict";
 
-let x, y, w, h;
-let r, g, b;
+// Not useful
+// let x, y, w, h;
+// let r, g, b;
 
 
 
@@ -20,9 +21,14 @@ function setup() {
     console.log("go")
     createCanvas(innerWidth, innerHeight);
     background(0);
-    drawEllipse(x = 80, y = 80, w = 50, h = 50, r = 255, g = 0, b = 0);
-    drawEllipse(x = 150, y = 150, w = 100, h = 100, r = 255, g = 144, b = 144);
-    drawEllipse(x = 250, y = 250, w = 150, h = 150, r = 255, g = 177, b = 177);
+
+    //drawEllipse(x = 80, y = 80, w = 50, h = 50, r = 255, g = 0, b = 0);
+    //drawEllipse(x = 150, y = 150, w = 100, h = 100, r = 255, g = 144, b = 144);
+    //drawEllipse(x = 250, y = 250, w = 150, h = 150, r = 255, g = 177, b = 177);
+
+    drawEllipse(80, 80, 50, 50, 255, 0, 0);
+    drawEllipse(150, 150, 100, 100, 255, 144, 144);
+    drawEllipse(250, 250, 150, 150, 255, 177, 177);
 }
 
 function draw() {
