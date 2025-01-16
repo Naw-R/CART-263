a4a9ccd1959299fb0d2e9a02a614a2bbae9562dc
Naw-R <rowannasser14@gmail.com>
2025-01-16 14:37:21 -0500

    Task 2 Completed

--- Tasks/Week 1/Task 2/js/script.js
+++ Tasks/Week 1/Task 2/js/script.js
@@ -1,10 +1,41 @@
+/**
+ * TASK 2
+ * --------------------------------
+ * INSTRUCTIONS : 
+ * REDO Task 1 but create and use a custom function with the following signature:
+ * function drawEllipse(x,y,w,h,r,g,b){ //body}
+ * Then: in the setup() make three calls to drawEllipse(...)
+ * --------------------------------
+ * CREATED BY : Rowan Nasser
+ */
+
 "use strict";
+
+let x, y, w, h;
+let r, g, b;
+
+
 
 function setup() {
     console.log("go")
-
+    createCanvas(innerWidth, innerHeight);
+    background(0);
+    drawEllipse(x = 80, y = 80, w = 50, h = 50, r = 255, g = 0, b = 0);
+    drawEllipse(x = 150, y = 150, w = 100, h = 100, r = 255, g = 144, b = 144);
+    drawEllipse(x = 250, y = 250, w = 150, h = 150, r = 255, g = 177, b = 177);
 }
 
 function draw() {
+    //Empty
+}
 
+
+function drawEllipse(x, y, w, h, r, g, b) {
+    push();
+    // No line around the shape
+    noStroke();
+    //Draw ellipse according to variables
+    fill(r, g, b);
+    ellipse(x, y, w, h);
+    pop();
 }
